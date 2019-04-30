e2a039b91593dc43372277b13ba2f2f4f2e35491
j8911 <j8911@LAPTOP-T93QK7TL>
2019-04-30 15:39:31 +0900

    부모클래스(Person)생성후 후손클래스(Man, BadBoy) 상속했습니다. 퍼즐에 넣을 이미지 9분할하여 넣어놨습니다

--- src/com/kh/project/model/dao/BadBoy.java
+++ src/com/kh/project/model/dao/BadBoy.java
@@ -1,5 +1,26 @@
 package com.kh.project.model.dao;
 
-public class BadBoy {
-
+public class BadBoy extends Person{
+	
+	public BadBoy(int hp, String kinds) {
+		super(hp, kinds);
+	}
+	
+	@Override
+	public void Hurt() {
+		System.out.println("으악!");
+	}
+	
+	public void attack() {
+		System.out.println("범인이 당신을 공격했습니다.");
+	}
+	
+	public void hitBadBoy() {
+		System.out.println("주인공의 hp가 10이 감소합니다.");
+		super.setHp(super.getHp() - 10);
+		
+		if(super.getHp() <= 0) {
+			System.out.println("당신은 방을 탈출하지 못했습니다.");
+		}
+	}
 }
--- src/com/kh/project/model/dao/Puzzle.java
+++ src/com/kh/project/model/dao/Puzzle.java
@@ -1,8 +1,143 @@
 package com.kh.project.model.dao;
 
-public class Puzzle {
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
+public class Puzzle extends JPanel implements ActionListener{
+
+	JButton[][] b = new JButton[3][3];
+	JButton b1 = new JButton("START");
+	JButton b2 = new JButton("EXIT");
+	
+	int[][] panCount = new int[3][3];
+	
+	int brow,bcol;
+	
 	public Puzzle() {
-		//전지용이 합니다
+		JPanel p1 = new JPanel();
+		p1.setLayout(new GridLayout(3,3,2,2));
+		
+		int k = 1;
+		
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				b[i][j] = new JButton(String.valueOf(k));
+				p1.add(b[i][j]);
+				b[i][j].addActionListener(this);
+				k++;
+			}
+		}
+		
+		JPanel p2 = new JPanel();
+		p2.add(b1);
+		p2.add(b2);
+		
+		setLayout(null);
+		p1.setBounds(150, 50, 340, 300);
+		p2.setBounds(150, 360, 340, 35);
+		
+		add(p1);
+		add(p2);
+		
+		b1.addActionListener(this);
+		b2.addActionListener(this);
 	}
+	
+	public void getRand() {
+		int[] num = new int[9];
+		int su = 0;
+		boolean bDash = false;
+		for(int i = 0; i < 9; i++) {
+			bDash = true;
+			while(bDash) {
+				su = (int)(Math.random() * 9);
+				bDash = false;
+				for(int j = 0; j < i; j++) {
+					if(num[j] == su) {
+						bDash = true;
+						break;
+					}
+				}
+			}
+			num[i] = su;
+			panCount[i/3][i%3] = su;
+			if(su == 8) {
+				brow = i/3;
+				bcol = i%3;
+			}
+		}
+	}
+	
+	public void display() {
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				if(i == brow && j == bcol) {
+					b[i][j].setEnabled(false);
+					b[i][j].setText("");
+				}else {
+					b[i][j].setEnabled(true);
+					b[i][j].setText(String.valueOf(panCount[i][j] + 1));
+				}
+			}
+		}
+	}
+	
+	public boolean isEnd() {
+		int k = 0;
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				if(panCount[i][j] != k)
+				return false;
+				k++;
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == b1) {
+			getRand();
+			display();
+		}
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				if(e.getSource() == b[i][j]) {
+					if(!(i == brow && Math.abs(bcol-j) == 1) || (j == bcol && Math.abs(brow - i) == 1))return;
+					panCount[brow][bcol] = panCount[i][j];
+					
+					panCount[i][j] = 8;
+					
+					brow = i;
+					bcol = j;
+					
+					display();
+					if(isEnd()) {
+						JOptionPane.showMessageDialog(this, "Game Over");
+					}
+				}
+			}
+		}
+	}
+
+public static void main(String[] args) {
+	new Puzzle();
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
