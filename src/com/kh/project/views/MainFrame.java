1bd075b5f34e7d020c7aa25565034950bab188c6
j8911 <j8911@LAPTOP-T93QK7TL>
2019-05-01 14:23:52 +0900

    퍼즐게임 완성했습니다.

--- src/com/kh/project/views/MainFrame.java
+++ src/com/kh/project/views/MainFrame.java
@@ -4,12 +4,11 @@ import javax.swing.JFrame;
 
 public class MainFrame extends JFrame{
 	{
-		
 		this.setSize(1024, 768);
 //		this.setLayout(null);
 //		new Start(this);
-		new Opening(this);
-		this.setTitle("食切実 害切実");
+//		new Opening(this);
+	this.setTitle("食切実 害切実");
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
